8acab86fc124fe587f54efa7fc756b3ef343c6a7
Cody Bontecou <bontecouc@gmail.com>
2024-06-22 17:43:46 +0800

    cool-kid graphic

--- pages/index.vue
+++ pages/index.vue
@@ -30,13 +30,19 @@ const projects = [
 <template>
   <div class="flex flex-col px-4 py-9 mx-auto w-full bg-stone-100">
     <Header class="mt-5 mb-10" />
-    <h1 class="text-[26px] font-lora font-normal mb-10">
-      Daniela Bontecou is a
-      <span class="text-[#D96B62]">UI designer</span> specializing in creating
-      impactful brand identities for seamless brand experiences.
-    </h1>
+    <div class="flex items-center py-10 gap-10">
+      <img
+        src="/cool-kid-standing.png"
+        class="hidden sm:block max-w-[270px] max-h-[250px]"
+      />
+      <h1 class="text-[26px] font-lora font-normal mb-10">
+        Daniela Bontecou is a
+        <span class="text-[#D96B62]">UI designer</span> specializing in creating
+        impactful brand identities for seamless brand experiences.
+      </h1>
+    </div>
 
-    <div class="grid sm:grid-cols-2 md:grid-cols-3 gap-5">
+    <div class="grid sm:grid-cols-2 gap-5">
       <ProjectCard
         v-for="project in projects"
         :link="project.link"
