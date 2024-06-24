49209da21b50b59e39d29a010e493f4b3c657e2f
Cody Bontecou <bontecouc@gmail.com>
2024-06-24 11:29:20 +0800

    moved main layout into app file

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <NuxtPage />
+  <div class="flex flex-col mx-auto bg-stone-100 max-w-[100%]">
+    <Header class="py-10 sm:bg-[#C3DECF] px-4 md:px-8" />
+    <div class="pb-10 px-4">
+      <NuxtPage />
+      <Footer />
+    </div>
   </div>
 </template>
