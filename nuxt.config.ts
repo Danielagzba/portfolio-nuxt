21d3957d6a8dbf52ba53908f6b2c52f977fc414e
Cody Bontecou <bontecouc@gmail.com>
2024-06-24 17:48:53 +0800

    removing unused head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    head: {},
-  },
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
