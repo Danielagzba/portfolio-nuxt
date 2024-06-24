841d06f21f412f5172fa66b440a082b7cb94837b
Cody Bontecou <bontecouc@gmail.com>
2024-06-24 11:29:40 +0800

    adapted for header/footer in app

--- pages/index.vue
+++ pages/index.vue
@@ -3,32 +3,25 @@ import { projects } from '~/lib/constants'
 </script>
 
 <template>
-  <div class="flex flex-col mx-auto w-full bg-stone-100">
-    <Header class="py-10 sm:bg-[#C3DECF] px-4 md:px-8" />
+  <div class="flex items-center py-10 gap-10">
+    <img
+      src="/cool-kid-standing.png"
+      class="hidden sm:block max-w-[270px] max-h-[250px]"
+    />
+    <h1 class="text-[26px] font-lora font-normal mb-10">
+      Daniela Bontecou is a
+      <span class="text-[#D96B62]">UI designer</span> specializing in creating
+      impactful brand identities for seamless brand experiences.
+    </h1>
+  </div>
 
-    <div class="pb-10 px-4">
-      <div class="flex items-center py-10 gap-10">
-        <img
-          src="/cool-kid-standing.png"
-          class="hidden sm:block max-w-[270px] max-h-[250px]"
-        />
-        <h1 class="text-[26px] font-lora font-normal mb-10">
-          Daniela Bontecou is a
-          <span class="text-[#D96B62]">UI designer</span> specializing in
-          creating impactful brand identities for seamless brand experiences.
-        </h1>
-      </div>
-
-      <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-y-5 gap-x-10">
-        <ProjectCard
-          v-for="project in projects"
-          :link="`/projects/${project.name}`"
-          :title="project.name"
-          :date="project.date"
-          :image="project.images[0]"
-        />
-      </div>
-      <Footer />
-    </div>
+  <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-y-5 gap-x-10">
+    <ProjectCard
+      v-for="project in projects"
+      :link="`/projects/${project.name}`"
+      :title="project.name"
+      :date="project.date"
+      :image="project.images[0]"
+    />
   </div>
 </template>
--- pages/projects/[...slug].vue
+++ pages/projects/[...slug].vue
@@ -20,63 +20,66 @@ onMounted(() => {
 </script>
 
 <template>
-  <div
-    class="flex flex-col px-4 py-9 mx-auto w-full bg-stone-100"
-    v-if="project"
-  >
-    <Header />
+  <div class="flex flex-col mt-10 text-neutral-700" v-if="project">
+    <h1
+      class="text-4xl font-bold text-primary-default gloria-hallelujah-regular"
+    >
+      {{ project.name }}
+    </h1>
+    <div class="mt-[50px] text-xl font-medium">
+      {{ project.description }}
+    </div>
+    <div class="mt-8">
+      <h2 class="font-semibold text-2xl">Tasks</h2>
+      <ul class="mt-2">
+        <li
+          v-for="task in project.tasks"
+          class="list-disc ml-4 font-medium mt-1"
+        >
+          {{ task }}
+        </li>
+      </ul>
+    </div>
 
-    <div class="flex flex-col mt-10 text-neutral-700">
-      <h1
-        class="text-4xl font-bold text-primary-default gloria-hallelujah-regular"
-      >
-        {{ project.name }}
-      </h1>
-      <div class="mt-[50px] text-xl font-medium">
-        {{ project.description }}
-      </div>
-      <div class="mt-8">
-        <h2 class="font-semibold text-2xl">Tasks</h2>
-        <ul class="mt-2">
-          <li
-            v-for="task in project.tasks"
-            class="list-disc ml-4 font-medium mt-1"
-          >
-            {{ task }}
-          </li>
-        </ul>
-      </div>
+    <div class="mt-8">
+      <h2 class="font-semibold text-2xl">Timeline</h2>
+      <p class="font-medium mt-2">{{ project.timeline }}</p>
+      <img class="mt-10" :src="project.images[0]" alt="designs on a phone" />
+    </div>
 
-      <div class="mt-8">
-        <h2 class="font-semibold text-2xl">Timeline</h2>
-        <p class="font-medium mt-2">{{ project.timeline }}</p>
-        <img class="mt-10" :src="project.images[0]" alt="designs on a phone" />
-      </div>
-
-      <div class="mt-8">
-        <h2 class="font-semibold text-2xl">Problem</h2>
-        <div class="font-medium text-[20px] leading-8 flex flex-col gap-3 mt-3">
-          <p v-for="problem in project.problems">
-            {{ problem }}
-          </p>
-        </div>
-      </div>
-
-      <div class="mt-10 flex flex-col gap-10">
-        <img :src="project.images[1]" alt="designs on a phone" />
-        <img :src="project.images[2]" alt="designs on a phone" />
-      </div>
-
-      <div class="mt-8">
-        <h2 class="font-semibold text-2xl">Solution</h2>
-        <div class="font-medium text-[20px] leading-8 flex flex-col gap-3 mt-3">
-          <p v-for="solution in project.solutions">
-            {{ solution }}
-          </p>
-        </div>
+    <div class="mt-8">
+      <h2 class="font-semibold text-2xl">Problem</h2>
+      <div class="font-medium text-[20px] leading-8 flex flex-col gap-3 mt-3">
+        <p v-for="problem in project.problems">
+          {{ problem }}
+        </p>
       </div>
     </div>
 
-    <Footer />
+    <div class="mt-10 flex gap-10 md:flex-row md:justify-between">
+      <div
+        class="mt-10 flex flex-col gap-4 md:flex-row md:justify-between md:w-full"
+      >
+        <img
+          src="/airbnb1.png"
+          alt="designs on a phone"
+          class="w-full md:w-[48%] object-contain"
+        />
+        <img
+          src="/airbnb1.png"
+          alt="designs on a phone"
+          class="w-full md:w-[48%] object-contain"
+        />
+      </div>
+    </div>
+
+    <div class="mt-8">
+      <h2 class="font-semibold text-2xl">Solution</h2>
+      <div class="font-medium text-[20px] leading-8 flex flex-col gap-3 mt-3">
+        <p v-for="solution in project.solutions">
+          {{ solution }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
