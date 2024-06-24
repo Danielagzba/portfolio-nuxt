74c29f851b3d7923002cba874ed357528a91f457
Cody Bontecou <bontecouc@gmail.com>
2024-06-24 12:52:53 +0800

    responsive about page

--- pages/about.vue
+++ pages/about.vue
@@ -7,10 +7,21 @@ const skills = [
   'Graphic Design',
   'Wireframing',
 ]
+
+const tools = [
+  'Figma',
+  'Adobe Illustrator',
+  'Adobe Lightroom',
+  'Adobe Premiere Pro',
+  'Canva',
+  'Spreadsheets',
+]
 </script>
 
 <template>
-  <div>
+  <div
+    class="max-w-screen-lg mx-auto flex flex-col gap-8 md:gap-[60px] lg:gap-[100px]"
+  >
     <div class="flex flex-col w-full items-center justify-center">
       <h1 class="font-semibold text-[26px] md:mt-[60px] md:text-[32px]">
         Get to know <span class="text-[#D96B62]">me</span> more
@@ -21,7 +32,7 @@ const skills = [
       />
     </div>
 
-    <div class="flex flex-col gap-4 font-medium leading-6 mt-10 md:text-xl">
+    <div class="flex flex-col gap-4 font-medium leading-6md:text-xl">
       <p>
         For the past years I've learned the skills to seamlessly created
         beautiful graphics. This approach ensures that user experiences are not
@@ -34,16 +45,27 @@ const skills = [
       </p>
     </div>
 
-    <div class="mt-8">
-      <h2 class="font-semibold text-2xl">Skills</h2>
-      <ul class="mt-2">
-        <li v-for="skill in skills" class="list-disc ml-4 font-medium mt-1">
-          {{ skill }}
-        </li>
-      </ul>
+    <div class="flex justify-around">
+      <div>
+        <h2 class="font-semibold text-2xl">Skills</h2>
+        <ul class="mt-2">
+          <li v-for="skill in skills" class="list-disc ml-4 font-medium mt-1">
+            {{ skill }}
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h2 class="font-semibold text-2xl">Tools</h2>
+        <ul class="mt-2">
+          <li v-for="tool in tools" class="list-disc ml-4 font-medium mt-1">
+            {{ tool }}
+          </li>
+        </ul>
+      </div>
     </div>
 
-    <div class="mt-8">
+    <div>
       <h2 class="font-semibold text-2xl">Aside from design</h2>
       <p class="font-medium mt-2">
         If I am not paddling out in the ocean with my husband, you'll find me
