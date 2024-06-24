3c756d040233f073972da400b6c0261df5952fff
Cody Bontecou <bontecouc@gmail.com>
2024-06-24 10:25:06 +0800

    product page mobile + variables

--- pages/airbnb.vue
+++ pages/airbnb.vue
@@ -1,72 +1,79 @@
+<script setup lang="ts">
+const projectName = 'Airbnb'
+const projectDescription =
+  'Airbnb is a platform that unlocks rewards for clothing donations and provides and up-cycled shopping experience.'
+const tasks = [
+  'Research',
+  'UI Design',
+  'Branding',
+  'Prototyping',
+  'Graphic Design',
+  'Usability Testing',
+]
+const timeline = 'Aug 2023 - Oct 2023'
+const problems = [
+  'I am a UI designer with a background in graphic design and photography.',
+  "I am passionate about creating intuitive and meaningful experiences that unite people and communities. Let's discuss user behavior, accessibility, and photography.",
+]
+const solutions = [
+  'I am a UI designer with a background in graphic design and photography.',
+  "I am passionate about creating intuitive and meaningful experiences that unite people and communities. Let's discuss user behavior, accessibility, and photography. nisi placerat. lobortis, dui Lorem dui eget id quam quis dolor eu sit Nunc hendrerit ultrices laoreet Donec lobortis, nulla, In Vestibulum ultrices Praesent",
+]
+const images = ['/airbnb1.png', '/airbnb1.png', '/airbnb1.png']
+</script>
+
 <template>
   <div class="flex flex-col px-4 py-9 mx-auto w-full bg-stone-100">
     <Header />
 
-    <div class="flex flex-col mt-10 bg-stone-100 text-neutral-700">
-      <div
+    <div class="flex flex-col mt-10 text-neutral-700">
+      <h1
         class="text-4xl font-bold text-primary-default gloria-hallelujah-regular"
       >
-        Airbnb
+        {{ projectName }}
+      </h1>
+      <div class="mt-[50px] text-xl font-medium">
+        {{ projectDescription }}
       </div>
-      <div class="mt-3 text-base">
-        The goal of this case study was to practice UI skills using Figma.
-        <br />During this UI Design Bootcamp I mastered my UI design skills in
-        Figma by practicing designing Airbnb's UI. <br />The goal of this case
-        study is to follow best practice for creating styles, components,
-        patterns, and a prototype that can be handed over to engineers.
+      <div class="mt-8">
+        <h2 class="font-semibold text-2xl">Tasks</h2>
+        <ul class="mt-2">
+          <li v-for="task in tasks" class="list-disc ml-4 font-medium mt-1">
+            {{ task }}
+          </li>
+        </ul>
       </div>
-    </div>
-    <img
-      loading="lazy"
-      srcSet="airbnbbanner.png"
-      class="mt-10 w-full md:w-auto"
-    />
-    <div class="flex flex-col mt-8 text-base bg-stone-100 text-neutral-800">
-      <div class="text-xl font-bold text-primary-light">Tasks</div>
-      <div class="mt-2">• Create scalable file structure in Figma</div>
-      <div class="mt-3">• Conduct UI benchmarking</div>
-      <div class="mt-3">• Create a auto layout wireframe in grey scale</div>
-      <div class="mt-3">• Define spacing variables</div>
-      <div class="mt-3">• Create responsive UI screen</div>
-      <div class="mt-3">• Create a series of components</div>
-      <div class="mt-3">• Define component properties</div>
-      <div class="mt-3">• Create color variables</div>
-      <div class="mt-3">• Add interaction to components</div>
-      <div class="mt-3">• Generate animations for specific componentss</div>
-    </div>
-    <div class="flex flex-col mt-8 bg-stone-100 text-neutral-800">
-      <div class="text-xl font-bold text-primary-light">Timeline</div>
-      <div class="mt-2 text-base">May 2024 - June 2024</div>
+
+      <div class="mt-8">
+        <h2 class="font-semibold text-2xl">Timeline</h2>
+        <p class="font-medium mt-2">{{ timeline }}</p>
+        <img class="mt-10" :src="images[0]" alt="designs on a phone" />
+      </div>
+
+      <div class="mt-8">
+        <h2 class="font-semibold text-2xl">Problem</h2>
+        <div class="font-medium text-[20px] leading-8 flex flex-col gap-3 mt-3">
+          <p v-for="problem in problems">
+            {{ problem }}
+          </p>
+        </div>
+      </div>
+
+      <div class="mt-10 flex flex-col gap-10">
+        <img :src="images[1]" alt="designs on a phone" />
+        <img :src="images[2]" alt="designs on a phone" />
+      </div>
+
+      <div class="mt-8">
+        <h2 class="font-semibold text-2xl">Solution</h2>
+        <div class="font-medium text-[20px] leading-8 flex flex-col gap-3 mt-3">
+          <p v-for="solution in solutions">
+            {{ solution }}
+          </p>
+        </div>
+      </div>
     </div>
 
-    <div class="mt-10 text-3xl font-bold text-neutral-700">Problem</div>
-    <div class="mt-3 text-2xl text-neutral-700">
-      I am a UI designer with a background in graphic design and photography.
-      <br /><br />I am passionate about creating intuitive and meaningful
-      experiences that unite people and communities. Let's discuss user
-      behavior, accessibility, and photography.
-    </div>
-    <img loading="lazy" srcSet="..." class="mt-10 w-full aspect-[0.98]" /><img
-      loading="lazy"
-      srcSet="..."
-      class="mt-10 w-full aspect-[0.98]"
-    /><img loading="lazy" srcSet="..." class="mt-10 w-full aspect-[0.98]" />
-    <div class="mt-10 text-3xl font-bold text-neutral-700">Solution</div>
-    <div class="mt-3 text-2xl text-neutral-700">
-      I am a UI designer with a background in graphic design and photography.
-      <br /><br />I am passionate about creating intuitive and meaningful
-      experiences that unite people and communities. Let's discuss user
-      behavior, accessibility, and photography. nisi placerat. lobortis, dui
-      Lorem dui eget id quam quis dolor eu sit Nunc hendrerit ultrices laoreet
-      Donec lobortis, nulla, In Vestibulum ultrices Praesent
-      <br /><br />faucibus eu libero, placerat ex efficitur. quis quam Ut
-      Quisque ex. Ut lorem. cursus placerat. nibh est. Ut malesuada ex lobortis,
-      porta tempor at, vel tempor <br /><br />nisi placerat. lobortis, dui Lorem
-      dui eget id quam quis dolor eu sit Nunc hendrerit ultrices laoreet Donec
-      lobortis, nulla, In Vestibulum ultrices Praesent <br /><br />faucibus eu
-      libero, placerat ex efficitur. quis quam Ut Quisque ex. Ut lorem. cursus
-      placerat. nibh est. Ut malesuada ex lobortis, porta tempor at, vel tempor.
-    </div>
     <Footer />
   </div>
 </template>
