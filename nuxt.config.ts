09f55cf331f0f8d85396a75c914b9fde939c177a
danielagzba <daniela.gbarron@gmail.com>
2024-06-26 20:20:07 +0800

    google

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,24 +2,25 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
-    '@nuxtjs/tailwindcss',
-    'nuxt-icon',
-    '@nuxt/fonts',
-    'shadcn-nuxt',
-    'nuxt-mail',
-    '@vueuse/nuxt',
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+    "@nuxt/fonts",
+    "shadcn-nuxt",
+    "nuxt-mail",
+    "@vueuse/nuxt",
+    "nuxt-gtag",
   ],
   runtimeConfig: {
     GMAIL_USERNAME: process.env.GMAIL_USERNAME,
     GMAIL_PASSWORD: process.env.GMAIL_PASSWORD,
   },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   mail: {
     message: {
-      to: 'danielagzba@gmail.com',
+      to: "danielagzba@gmail.com",
     },
     smtp: {
-      host: 'smtp.gmail.com',
+      host: "smtp.gmail.com",
       port: 465,
       auth: {
         user: process.env.GMAIL_USERNAME,
@@ -27,4 +28,7 @@ export default defineNuxtConfig({
       },
     },
   },
-})
+  gtag: {
+    id: "G-06DHNR8LFF",
+  },
+});
